e1827a252a1d095954acdc800eb9579d7235aef5
jpi nodobtc <jp.cr3spo@pm.me>
2022-11-08 10:52:30 -0400

    obteniendo el % de sync

--- Python-Scripts/test.py
+++ Python-Scripts/test.py
@@ -1,11 +1,15 @@
 #from dotenv import load_dotenv
 from bitcoin.rpc import RawProxy
 from pprint import pprint
+#RPC remote procedure call
+
 p = RawProxy()
 
 info = p.getblockchaininfo()
 
-pprint(info)
+sync = float(info['verificationprogress'])
+
+print(round(sync*100,5))
 
 
 #path = '/home/ghost/rpibots/'
