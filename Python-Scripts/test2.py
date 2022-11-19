9f7a0a90684ac996a54f7320222cc5ffd78cdc87
jpi nodobtc <jp.cr3spo@pm.me>
2022-11-18 22:40:08 -0400

    rpi status

--- Python-Scripts/test2.py
+++ Python-Scripts/test2.py
@@ -4,7 +4,8 @@ rpc_user='raspibolt'
 rpc_password='2453889'
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(rpc_user, rpc_password))
-best_block_hash = rpc_connection.getblockchaininfo()
-print(best_block_hash)
+
+best = rpc_connection.getblockchaininfo()
+print(best['verificationprogress'])
 
 # batch support : print timestamps of blocks 0 to 99 in 2 RPC round-trips:
