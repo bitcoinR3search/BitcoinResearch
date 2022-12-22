da90b9838fd629673b1bc6276f1a62ccd99e23f8
jpi nodobtc <jp.cr3spo@pm.me>
2022-12-22 06:25:40 -0400

    actualizando core

--- Python-Scripts/test2.py
+++ Python-Scripts/test2.py
@@ -1,7 +1,7 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
-rpc_user='raspibolt'
-rpc_password='2453889'
+rpc_user='ghost'
+rpc_password='2453889Crespo'
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(rpc_user, rpc_password))
 
