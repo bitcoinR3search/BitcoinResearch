c13d7790dbd9a15841e138eb85ca496b54b10e2e
Juan.py <jp.crespo.vargas@gmail.com>
2023-03-09 12:40:46 -0400

    yml

--- Onchain/scripts/updateprice.py
+++ Onchain/scripts/updateprice.py
@@ -3,12 +3,16 @@
 
 import json, requests, datetime
 
+def func():
+   now = datetime.datetime.now()
 
-now = datetime.datetime.now()
+   url_req = requests.get('http://api.coindesk.com/v1/bpi/currentprice.json')
+   data = url_req.json()['bpi']['USD']['rate']
+   data_clean = float(data.replace(',', ''))
+   print(round(data_clean,2),end=' ')
+   print('USD',end=',')
+   print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
-url_req = requests.get('http://api.coindesk.com/v1/bpi/currentprice.json')
-data = url_req.json()['bpi']['USD']['rate']
-data_clean = float(data.replace(',', ''))
-print(round(data_clean,2),end=' ')
-print('USD',end=',')
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+if __name__== "__main__":
+   func()
