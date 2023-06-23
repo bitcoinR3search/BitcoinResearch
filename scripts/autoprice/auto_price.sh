a988a938d07da229d43e7bc957766f0ffd1a4225
nodetwo <jp.cr3spo@pm.me>
2023-06-23 11:03:12 -0400

    auto price 2023-06-23

--- scripts/autoprice/auto_price.sh
+++ scripts/autoprice/auto_price.sh
@@ -1,6 +1,7 @@
 cd /home/ghost/BitcoinResearch/BitcoinResearch/scripts/bins
 
-git pull origin main
+
 git add -A
 git commit -m "auto price $(date +'%Y-%m-%d')"
+git pull -ff origin main
 git push origin main
