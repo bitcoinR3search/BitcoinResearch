2798f00507b46caa64b19be0cc12bcf07256594c
Juan.py <jp.crespo.vargas@gmail.com>
2023-10-02 22:39:49 -0400

    ál dia

--- scripts/analisis/app/readata.py
+++ scripts/analisis/app/readata.py
@@ -4,8 +4,8 @@ import pandas as pd
 import matplotlib.dates as mdates
 
 
-#os.chdir('D:/proyectos/BitcoinResearch//BitcoinResearch/scripts/')
-os.chdir('/home/ghost/BitcoinResearch/scripts/')
+os.chdir('D:/proyectos/BitcoinResearch//BitcoinResearch/scripts/')
+#os.chdir('/home/ghost/BitcoinResearch/scripts/')
 
 
 def leer_data(*args):
