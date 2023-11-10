08ba66b7063a9b40dfaef6ebde1aa83da977d9f2
nodeone <jp.cr3spo@pm.me>
2023-11-10 02:36:00 -0400

    actualización semanal

--- scripts/analisis/SUBIDA.sh
+++ scripts/analisis/SUBIDA.sh
@@ -2,6 +2,10 @@
 cd /home/ghost/BitcoinResearch/
 
 git pull --rebase origin main
+
+source /home/ghost/dev-env/bin/activate
+python block_size.py && python supplybtc.py 
+
 git add -A
 git commit -m 'actualización semanal'
 git push origin main
