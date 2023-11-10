fa66ceb26825baef63eeadc64606ad9d578958ff
nodeone <jp.cr3spo@pm.me>
2023-11-09 23:21:38 -0400

    actualización semanal

--- scripts/hello world/helloworld2.py
+++ scripts/hello world/helloworld2.py
@@ -8,20 +8,21 @@ from bitcoinrpc.authproxy import AuthServiceProxy
 from dotenv import load_dotenv
 from pprint import pprint
 
-os.chdir('D:\proyectos\BitcoinResearch\BitcoinResearch\scripts')
+#os.chdir('D:\proyectos\BitcoinResearch\BitcoinResearch\scripts')
+os.chdir('/home/ghost/BitcoinResearch/scripts')
 
-#load_dotenv('/home/ghost/.env')
-load_dotenv('analisis/.env')
+load_dotenv('/home/ghost/.env')
+#load_dotenv('analisis/.env')
 
 
 # RPC remote procedure call
 rpc_user = os.getenv('user')
 rpc_password = os.getenv('pass')
 
+
 # rpc_user and rpc_password are set in the bitcoin.conf file
 
-rpc_connection = AuthServiceProxy(
-    "http://%s:%s@nodeone.local:8332" % (rpc_user, rpc_password))
+rpc_connection = AuthServiceProxy("http://%s:%s@nodeone.local:8332" % (rpc_user, rpc_password))
 
 best = rpc_connection.getblockchaininfo()
 pprint(best)
